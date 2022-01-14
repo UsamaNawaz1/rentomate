308ba1e599e32740932381fff767d99fc9d2ee80
UsamaNawaz1 <usamanawaz1994@gmail.com>
2022-01-15 02:45:16 +0500

    adding JWT Authentication

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name='routes'),
+    
+
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/profile/', views.getUserProfile, name='users-profile'),
+    path('users/', views.getUsers, name='users'),
+    path('users/register/', views.registerUser, name='register'),
+
     path('properties/', views.getProperties, name='properties'),
     path('property/<str:pk>/', views.getProperty, name='property'),
 ]
